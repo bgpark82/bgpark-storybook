aa413c6db7ccab80d692deea948fd1da37135b09
bgpark <bgpark@citus.co.kr>
2020-02-27 12:07:12 +0900

    Create theme on button

--- src/Button/Button.stories.tsx
+++ src/Button/Button.stories.tsx
@@ -1,6 +1,7 @@
+/** @jsx jsx */
 import { withKnobs } from '@storybook/addon-knobs';
-import React from 'react';
 import Button from './Button';
+import {jsx, css} from '@emotion/core'
 
 export default {
     title : 'component|button',
@@ -14,3 +15,28 @@ export const hello = () => {
         <Button>BUTTON</Button>
     )
 }
+
+export const themes = () => {
+    return(
+        <div css={buttonWrapper}>
+            <div>
+                <Button theme="primary">PRIMARY</Button>
+            </div>
+            <div>
+                <Button theme="secondary">SECONDARY</Button>
+            </div>
+            <div>
+                <Button theme="tertiary">TERTIARY</Button>
+            </div>
+        </div>
+    )
+}
+
+
+
+const buttonWrapper = css`
+    display:flex;
+    & > div {
+        margin-right:1rem;
+    }
+`
--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,13 +1,80 @@
-import React from 'react';
+/** @jsx jsx */
+import {jsx, css} from '@emotion/core'
 
 type ButtonProps = {
-    children: React.ReactNode
+    /** 버튼 안의 내용 */
+    children: React.ReactNode,
+    /** 버튼의 theme */
+    theme: 'primary' | 'secondary' | 'tertiary',
 }
 
-const Hello = ({children}:ButtonProps) => {
+const Button = ({children, theme}:ButtonProps) => {
     return(
-        <button>{children}</button>
+        <button css={[style, themes[theme]]}>{children}</button>
     )
 }
 
-export default Hello;
+Button.defaultProps = {
+    theme: 'primary',
+    size: 'medium'
+}
+
+const style = css`
+    outline:none;
+    border:none;
+    box-sizing: border-box;
+    height:2rem;
+    font-size:0.875rem;
+    padding: 0.5rem;
+    background: #20c997;
+    color:white;
+    border-radius:0.25rem;
+    line-height:1;
+    font-weight:600;
+    &:focus {
+        box-shadow: 0px 0px 6px rgba(0,0,0,0.2);
+    }
+    &:hover {
+        background: #38d9a9;
+    }
+    &:active{
+        background: #12b886;
+    }
+`
+
+const themes = {
+    primary : css`
+        background: #20c997;
+        color:white;
+        &:hover {
+        background: #38d9a9;
+        }
+        &:active{
+            background: #12b886;
+        }
+    `,
+    secondary: css`
+        background: #e9ecef;
+        color: #343a40;
+        &:hover {
+            background: #f1f3f5;
+        }
+        &:active {
+            background: #dee2e6;
+        }
+    `,
+    tertiary: css`
+        background: none;
+        color: #20c997;
+        &:hover {
+            background: #e6fcf5;
+        }
+        &:active {
+            background: #c3fae8;
+        }
+    `
+}
+
+
+
+export default Button;
